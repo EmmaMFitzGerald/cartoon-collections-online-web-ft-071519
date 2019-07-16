130811856fa708ad883969bea9a8b3d2a6806e63
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-16 12:48:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(array, cheese_types)
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
     cheese_types.include?(item) 
